197eca0a89989cfd2f38bd0dcf58f60b08a1712c
Miguel Alvarez <malvarez@uni-bonn.de>
2016-04-28 21:55:54 +0200

    class dfplus

---
+++ R/dfplus-class.R
@@ -0,0 +1,20 @@
+# TODO:   Set a S4 object for added curves
+# 
+# Author: Miguel Alvarez
+################################################################################
+
+setClass("dfplus", slots=c(
+				data="data.frame",
+				metadata="data.frame"),
+		prototype=list(
+                data=data.frame(),
+                metadata=data.frame(LongName=character(), Type=character(),
+                        Units=character(), Values=character(),
+                        Description=character(), stringsAsFactors=FALSE)),
+		validity=function(object) {
+			if(!all(colnames(object@data) == rownames(object@variables)))
+				return("column names in slot 'data' do not match row names in slot 'metadata'")
+			if(!all(c("LongName","Type","Units","Values","Description") %in%
+							names(object@variables)))
+				return("column names LongName, Type, Units, Values, and Description are mandatory in slot 'metadata'")
+		})
