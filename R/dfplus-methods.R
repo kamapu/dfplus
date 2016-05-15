f2ae08710ce60aefc12b6cfa2ec894c3cb88feff
Miguel Alvarez <malvarez@uni-bonn.de>
2016-05-15 13:49:35 +0200

    First methods for dfplus
    head and tail

--- R/dfplus-methods.R
+++ R/dfplus-methods.R
@@ -1,9 +1,9 @@
-# TODO:   Some ways to access and set content of dfplus objects
+# TODO:   Some methods specific to dfplus objects
 # 
 # Author: Miguel Alvarez
 ################################################################################
 
-# 1: Access and replace methods for dollar symbol ------------------------------
+# Access and replace methods for dollar symbol ---------------------------------
 setMethod("$", signature(x="dfplus"), function(x, name) {
             x@data[[name]]
         }
@@ -16,6 +16,50 @@ setReplaceMethod("$", signature(x="dfplus"), function(x, name, value) {
         }
 )
 
-# 2: Access and replace methods for square brakets -----------------------------
+# Access and replace methods for square brakets --------------------------------
+setMethod("[", signature(x="dfplus"), function(x, i, j, ..., drop=FALSE) {
+            if(missing(i)) i <- TRUE
+            if(missing(j)) j <- TRUE
+            # Resolving problems with NAs
+            if(is.logical(i)) i[is.na(i)] <- FALSE else i <- na.omit(i)
+            if(is.logical(j)) i[is.na(j)] <- FALSE else j <- na.omit(j)
+            x@data <- x@data[i,j,drop]
+            x@metadata <- x@metadata[j,,drop]
+            return(x)
+        }
+)
 
-# 3: Access and replace methods for function names -----------------------------
+setReplaceMethod("[", signature(x="dfplus"), function(x, i, j, value) {
+            if(missing(i)) i <- TRUE
+            if(missing(j)) j <- TRUE
+            # Resolving problems with NAs
+            if(is.logical(i)) i[is.na(i)] <- FALSE else i <- na.omit(i)
+            if(is.logical(j)) i[is.na(j)] <- FALSE else j <- na.omit(j)
+            x@data[i,j] <- value
+            return(x)
+        }
+)
+
+# head and tail methods --------------------------------------------------------
+setMethod("head", signature(x="dfplus"), function(x, n=6L, ...) {
+            head(x@data, n=n, ...)
+        }
+)
+
+setMethod("tail", signature(x="dfplus"), function(x, n=6L, ...) {
+            tail(x@data, n=n, ...)
+        }
+)
+
+# names ------------------------------------------------------------------------
+setMethod("names", signature(x="dfplus"), function(x) {
+            names(x@data)
+        }
+)
+
+setReplaceMethod("names", signature(x="dfplus"), function(x, value) {
+            names(x@data) <- value
+            rownames(x@metadata) <- value
+            return(x)
+        }
+)
