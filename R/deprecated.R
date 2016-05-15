475a245d916befb2c4a239498e0507c9a02646f1
Miguel Alvarez <malvarez@uni-bonn.de>
2016-05-15 14:20:26 +0200

    New methods for dfplus objects
    deprecated and skip_deprecated

---
+++ R/deprecated.R
@@ -0,0 +1,31 @@
+# TODO:   Some functions to handle deprecated variables in dfplus objects
+# 
+# Author: Miguel Alvarez
+################################################################################
+
+# deprecated -------------------------------------------------------------------
+setGeneric("deprecated", function(x, ...)
+            standardGeneric("deprecated"))
+
+setMethod("deprecated", signature(x="dfplus"),
+        function(x, ...) return(x@metadata$Type == "deprecated")
+)
+
+# replacement method
+setGeneric("deprecated<-", function(x, value)
+            standardGeneric("deprecated<-"))
+
+setReplaceMethod("deprecated", signature(x="dfplus", value="character"),
+        function(x, value) {
+            x@metadata[value, "Type"] <- "deprecated"
+            return(x)
+        }
+)
+
+# skip_deprecated --------------------------------------------------------------
+setGeneric("skip_deprecated", function(x, ...)
+            standardGeneric("skip_deprecated"))
+
+setMethod("skip_deprecated", signature(x="dfplus"),
+        function(x, ...) return(x[,!deprecated(x)])
+)
